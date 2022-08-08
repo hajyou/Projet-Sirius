1d1244234b3ab5ed18b9362226b8e72f68077fb9
Hajri <110405736+hajyou@users.noreply.github.com>
2022-08-08 13:04:52 +0100

    Add files via upload

---
+++ Dépenses/main_dépenses.py
@@ -0,0 +1,88 @@
+#coding:utf-8
+
+import sqlite3
+import os
+import time
+
+boucles1 = True
+mv = 0
+
+while boucles1 == True:
+    log = "test"
+    login = input("Entrez votre mot de passe \n>>")
+    if login != log:
+        print("Ce n'est pas le bon mot de passe !")
+        time.sleep(2)
+        continue
+    else:
+        pass
+
+    print("\n-------------- Version 1.0 --------------")
+    print("[SYSTEM Y.H ] : Programme (Dépenses) lancé !")
+    print("_________________________________________\n")
+    boucles = True
+    connection = sqlite3.connect("base.db")
+    cursor = connection.cursor()
+
+    while boucles == True:
+
+        new_values = (time.strftime("%d/%m/%Y"), input("\nEntrer votre désignation \n>>"), float(input("\nEntrer votre dépense \n>>")))
+
+        cursor.execute('INSERT INTO tt_donnees VALUES(?, ?, ?)', new_values)
+        connection.commit()
+        req2 = cursor.execute('SELECT * FROM tt_donnees')
+        print("\n_________Base de données__________\n")
+        for row in req2.fetchall():
+            print(row)
+            print("\n__________________________________\n")
+        demande = input("Voulez-vous |nettoyer| votre base de données ? o/n \n>>")
+        if demande == "o":
+            verif = input("Etes-vous sûrs de vouloir nettoyer votre base de données ? o/n ?\ncela effacera toutes les données !\n>>")
+            while 1:    
+                if verif == 'o':
+                    req = 'DELETE FROM tt_donnees'
+                    cursor.execute(req)
+                    connection.commit()
+                    time.sleep(1)
+                    print("Votre base de données a bien été nettoyer .. \n")
+                    print("\n_____________________ Base de données ___________________________\n")
+                    print("\n____________  Vide (Tapez Ctrl + z pour quitter !)  _____________\n")
+                    time.sleep(2)
+                    break
+                    while 1:   
+                        mess2 = input("Avez-vous d'autres données ? y/n\n>>")
+                        if mess2 == 'y':
+                            boucles = True
+                            break
+                        elif mess2 == 'n':
+                            print("Fermeture en cours ...")
+                            time.sleep(2)
+                            connection.close()
+                            boucles1 = False
+                            break
+                        else:
+                            print("Vous devez soit confirmer par 'y' pour oui soit par 'n' pour non")
+                            continue
+                elif verif == 'n':
+                    break
+                else:
+                    print("Vous vous étes surment tromper retaper soit 'n' pour non soit 'o' pour oui")
+                    continue
+        else:
+            while mv == 0:
+                mess = input("Avez-vous d'autres données ? y/n")
+                if mess == 'y':
+                    boucles = True
+                    mv = 1
+                elif mess == 'n':
+                    print("Fermeture en cours ...")
+                    time.sleep(2)
+                    connection.close()
+                    boucles1 = False
+                    mv = 1
+                else:
+                    print("Vous devez soit confirmer par 'y' pour oui soit par 'n' pour non")
+                    continue
+
+
+os.system('pause')
